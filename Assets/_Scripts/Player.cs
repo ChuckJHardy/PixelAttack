54752d50a4f182c3934899630ea3b23cb89f1571
Charles J Hardy <chuckjhardy@gmail.com>
2015-01-12 19:09:52 +0000

    Add Up Controls to Player

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -3,26 +3,43 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 	public float speed = 10f;
+	public float jetSpeed = 15f;
 	public Vector2 maxVelocity = new Vector2 (3, 5);
+	public float airSpeedMultiplier = 0.3f;
+
+	bool standing;
 
 	void Update () {
 		float forceX = 0f;
 		float forceY = 0f;
 
 		float absVelocityX = Mathf.Abs(rigidbody2D.velocity.x);
+		float absVelocityY = Mathf.Abs(rigidbody2D.velocity.y);
+
+		if (absVelocityY < 0.2f) {
+			standing = true;
+		} else {
+			standing = false;
+		}
 
 		if (Input.GetKey("right")) {
 			if (absVelocityX < maxVelocity.x) {
-				forceX = speed;
+				forceX = standing ? speed : (speed * airSpeedMultiplier);
 			}
 
 			transform.localScale = new Vector3 (1, 1, 1);
 		} else if (Input.GetKey("left")) {
 			if (absVelocityX < maxVelocity.x) {
-				forceX = -speed;
+				forceX = standing ? -speed : (-speed * airSpeedMultiplier);
 			}
 
 			transform.localScale = new Vector3 (-1, 1, 1);
+		}
+
+		if (Input.GetKey("up")) {
+			if (absVelocityY < maxVelocity.y) {
+				forceY = jetSpeed;
+			}
 		}
 
 		rigidbody2D.AddForce(new Vector2 (forceX, forceY));
