b4e14bbcab91725d119ff154c498e7b4abd15ea9
Charles J Hardy <chuckjhardy@gmail.com>
2015-01-13 18:21:40 +0000

    Add Deadly Small Spikes

---
+++ Assets/_Scripts/Explode.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explode : MonoBehaviour {
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.tag == "Deadly") OnExplode();
+	}
+
+	void OnExplode () {
+		Destroy (gameObject);
+	}
+}
