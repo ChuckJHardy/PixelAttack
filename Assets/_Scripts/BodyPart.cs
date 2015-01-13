6a8b6fc9726fa99903851c0767ba06dfe242ba91
Charles J Hardy <chuckjhardy@gmail.com>
2015-01-13 18:55:28 +0000

    Fade out Body Parts on Explode

---
+++ Assets/_Scripts/BodyPart.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BodyPart : MonoBehaviour {
+	
+	private SpriteRenderer spriteRenderer;
+	private Color start;
+	private Color end;
+	private float time = 0.0f;
+	
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		start = spriteRenderer.color;
+		end = new Color (start.r, start.g, start.b, 0.0f);
+	}
+	
+	void Update () {
+		time += Time.deltaTime;
+		
+		renderer.material.color = Color.Lerp (start, end, time / 2);
+		
+		if (renderer.material.color.a <= 0.0) {
+			Destroy (gameObject);
+		}
+	}
+}
--- Assets/_Scripts/Explode.cs
+++ Assets/_Scripts/Explode.cs
@@ -2,11 +2,24 @@
 using System.Collections;
 
 public class Explode : MonoBehaviour {
+	public BodyPart bodyPart;
+	public int totalBodyParts = 5;
+
 	void OnTriggerEnter2D (Collider2D other) {
 		if (other.gameObject.tag == "Deadly") OnExplode();
 	}
 
 	void OnExplode () {
 		Destroy (gameObject);
+
+		var currentTransform = transform;
+
+		for (int i = 0; i < totalBodyParts; i++) {
+			currentTransform.TransformPoint(0, -100, 0);
+			BodyPart clone = Instantiate(bodyPart, currentTransform.position, Quaternion.identity) as BodyPart;
+
+			clone.rigidbody2D.AddForce (Vector2.right * Random.Range(-50, 50));
+			clone.rigidbody2D.AddForce (Vector2.up * Random.Range(100, 400));
+		}
 	}
 }
