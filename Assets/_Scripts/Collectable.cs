810059c10b54d908c2797a5ef08ed3cb23d0ac32
Charles J Hardy <chuckjhardy@gmail.com>
2015-01-13 15:23:42 +0000

    Simple Crystal Collectable

---
+++ Assets/_Scripts/Collectable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectable : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			Destroy (gameObject);
+		}
+	}
+}
