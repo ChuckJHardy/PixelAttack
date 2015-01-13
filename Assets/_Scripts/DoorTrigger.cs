e63ac3078b45f49d18ecb990f480ec5d55547262
Charles J Hardy <chuckjhardy@gmail.com>
2015-01-13 18:11:39 +0000

    Door Trigger Gizmos -> Trigger Wire Cude

--- Assets/_Scripts/DoorTrigger.cs
+++ Assets/_Scripts/DoorTrigger.cs
@@ -21,4 +21,15 @@ public class DoorTrigger : MonoBehaviour {
 			door.CloseWithDelay();
 		}
 	}
+
+	void OnDrawGizmos () {
+		Gizmos.color = ignoreTrigger ? Color.grey : Color.green;
+
+		var bc2d = GetComponent<BoxCollider2D> ();
+		var bcPos = bc2d.transform.position;
+		var newPos = new Vector2 (bcPos.x + bc2d.center.x, bcPos.y + bc2d.center.y);
+		var size = new Vector2 (bc2d.size.x, bc2d.size.y);
+
+		Gizmos.DrawWireCube(newPos, size);
+	}
 }
