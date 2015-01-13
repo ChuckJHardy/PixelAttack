485f05fb909250707c5229e4acf8dbae07b892f7
Charles J Hardy <chuckjhardy@gmail.com>
2015-01-13 16:25:16 +0000

    Switch

---
+++ Assets/_Scripts/Switch.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : MonoBehaviour {
+
+	public Animator animator;
+
+	void Awake () {
+		animator = GetComponent<Animator> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		animator.SetInteger ("AnimationState", 1);
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		animator.SetInteger ("AnimationState", 2);
+	}
+}
