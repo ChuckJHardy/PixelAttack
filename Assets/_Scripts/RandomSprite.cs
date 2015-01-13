61e0c91368b219849e771e267934b3e4d2f589fa
Charles J Hardy <chuckjhardy@gmail.com>
2015-01-13 15:52:56 +0000

    Randomly Generate Crystal from Sprite

---
+++ Assets/_Scripts/RandomSprite.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSprite : MonoBehaviour {
+
+	public Sprite[] sprites;
+	public string resourceName;
+	public int currentSpriteIndex = -1;
+
+	private SpriteRenderer spriteRenderer;
+
+	void Awake () {
+		sprites = Resources.LoadAll<Sprite> (resourceName);	
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+
+	void Start () {
+		int randomNumber = Random.Range (0, sprites.Length);
+
+		if (currentSpriteIndex < 0) {
+			currentSpriteIndex = randomNumber;
+		} else if (currentSpriteIndex > sprites.Length) {
+			currentSpriteIndex = sprites.Length - 1;
+		}
+
+		spriteRenderer.sprite = sprites[currentSpriteIndex];
+	}
+}
