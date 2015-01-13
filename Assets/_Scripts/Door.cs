cf5428ec5717921cbde51a5b629bb234b8fcdda9
Charles J Hardy <chuckjhardy@gmail.com>
2015-01-13 16:53:26 +0000

    Add Door Open/Close Trigger

---
+++ Assets/_Scripts/Door.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Open() {
+		Debug.Log ("Opening Door");
+	}
+
+	public void Close() {
+		Debug.Log ("Closing Door");
+	}
+}
---
+++ Assets/_Scripts/DoorTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTrigger : MonoBehaviour {
+
+	public Door door;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			door.Open();
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			door.Close();
+		}
+	}
+}
