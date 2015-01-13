62e6a934180d88d5822a6f9382cfaec6c86868a6
Charles J Hardy <chuckjhardy@gmail.com>
2015-01-13 19:27:29 +0000

    Move AlienA Forward

---
+++ Assets/_Scenes/MoveForward.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveForward : MonoBehaviour {
+	public float speed = 0.5f;
+
+	void Update () {
+		rigidbody2D.velocity = new Vector2 (transform.localScale.x * speed, 0);
+	}
+}
