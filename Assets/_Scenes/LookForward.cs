4290eab94f07f8af9d69408cbb950031fb0df297
Charles J Hardy <chuckjhardy@gmail.com>
2015-01-13 19:36:20 +0000

    Turn Alien A when a Solid is Seen

---
+++ Assets/_Scenes/LookForward.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookForward : MonoBehaviour {
+	public Transform sightStart, sightEnd;
+
+	private bool collision = false;
+
+	void Update () {
+		collision = Physics2D.Linecast (sightStart.position, sightEnd.position, 1 << LayerMask.NameToLayer ("Solid"));
+		Debug.DrawLine(sightStart.position, sightEnd.position, Color.green);
+
+		if (collision) {
+			int posX = transform.localScale.x == 1 ? -1 : 1;
+			this.transform.localScale = new Vector3(posX, 1, 1);
+		}
+	}
+}
