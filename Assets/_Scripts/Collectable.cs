affdee51cf8038622d5888f5b91061608e98cb0e
Charles J Hardy <chuckjhardy@gmail.com>
2015-01-14 18:46:35 +0000

    Collectable Sounds

--- Assets/_Scripts/Collectable.cs
+++ Assets/_Scripts/Collectable.cs
@@ -2,8 +2,11 @@
 using System.Collections;
 
 public class Collectable : MonoBehaviour {
+	public AudioClip pickupSound;
+
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.tag == "Player") {
+			AudioSource.PlayClipAtPoint(pickupSound, transform.position);
 			Destroy (gameObject);
 		}
 	}
