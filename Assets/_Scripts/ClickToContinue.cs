4256988be7af96df5757003260c5f4e05266582d
Charles J Hardy <chuckjhardy@gmail.com>
2015-01-14 19:15:31 +0000

    Load first Level from Splash Screen

---
+++ Assets/_Scripts/ClickToContinue.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToContinue : MonoBehaviour {
+	public string sceneName;
+
+	bool loadLock;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		if (Input.GetMouseButtonDown(0) && !loadLock) {
+			LoadLevel();
+		}	
+	}
+
+	void LoadLevel() {
+		loadLock = true;
+		Application.LoadLevel(sceneName);
+	}
+}
