1e3d79e57637cc31955f4b33e9a1508ad531b089
Charles J Hardy <chuckjhardy@gmail.com>
2015-01-14 19:24:28 +0000

    Add Exit Sign for Level

---
+++ Assets/_Scripts/ExitSign.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitSign : MonoBehaviour {
+	public string sceneName;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			Destroy(other.gameObject);
+			LoadLevel();
+		}
+	}
+
+	void LoadLevel() {
+		Application.LoadLevel(sceneName);
+	}
+}
