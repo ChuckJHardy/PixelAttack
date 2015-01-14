61508a31582574da77ca59ba21e9b0bb014e8d60
Charles J Hardy <chuckjhardy@gmail.com>
2015-01-14 15:39:23 +0000

    Alien A aware of gaps

--- Assets/_Scripts/LookForward.cs
+++ Assets/_Scripts/LookForward.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class LookForward : MonoBehaviour {
 	public Transform sightStart, sightEnd;
+	public bool needsCollision = true;
 
 	bool collision = false;
 
@@ -10,7 +11,7 @@ public class LookForward : MonoBehaviour {
 		collision = Physics2D.Linecast (sightStart.position, sightEnd.position, 1 << LayerMask.NameToLayer ("Solid"));
 		Debug.DrawLine(sightStart.position, sightEnd.position, Color.green);
 
-		if (collision) {
+		if (needsCollision == collision) {
 			int posX = transform.localScale.x == 1 ? -1 : 1;
 			this.transform.localScale = new Vector3(posX, 1, 1);
 		}
