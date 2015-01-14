33ea3dbe2e86eac2da027557590488754fe01495
Charles J Hardy <chuckjhardy@gmail.com>
2015-01-14 16:03:08 +0000

    Animate AlienB to Attack on Trigger

---
+++ Assets/_Scripts/AlienB.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlienB : MonoBehaviour {
+	private Animator animator;
+
+	void Awake () {
+		animator = GetComponent<Animator>();
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		animator.SetInteger("AnimationState", 1);
+	}
+}
