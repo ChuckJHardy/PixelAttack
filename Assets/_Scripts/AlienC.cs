0842739f90044d5f70fe36cb2cb0cba489f17064
Charles J Hardy <chuckjhardy@gmail.com>
2015-01-14 16:41:09 +0000

    Alien C Animation

---
+++ Assets/_Scripts/AlienC.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlienC : MonoBehaviour {
+
+	public float attackDelay = 3f;
+
+	Animator animator;
+
+	void Start () {
+		animator = GetComponent<Animator> ();			
+
+		if (attackDelay > 0) {
+			StartCoroutine(OnAttack());
+		}
+	}
+	
+	void Update () {
+		animator.SetInteger("AnimationState", 0);	
+	}
+
+	IEnumerator OnAttack() {
+		yield return new WaitForSeconds(attackDelay);
+		Fire();
+		StartCoroutine(OnAttack());
+	}
+
+	void Fire() {
+		animator.SetInteger("AnimationState", 1);	
+	}
+}
