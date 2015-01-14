d38691e887c3cda9dd8d13814d3068f6b9acb6b6
Charles J Hardy <chuckjhardy@gmail.com>
2015-01-14 17:29:42 +0000

    Camera should follow Player

---
+++ Assets/_Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject target;
+
+	Transform targetTransform;
+
+	void Awake() {
+		camera.orthographicSize = ((Screen.height / 2.0f) / 100f);
+	}
+
+	void Start () {
+		targetTransform = target.transform;	
+	}
+	
+	void Update () {
+		transform.position = new Vector3(targetTransform.position.x, targetTransform.position.y, transform.position.z);			
+	}
+}
