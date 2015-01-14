02222da276fdb46825add3248a3b40c3567cadc1
Charles J Hardy <chuckjhardy@gmail.com>
2015-01-14 19:09:47 +0000

    Splash Screen

---
+++ Assets/_Scripts/FloatEffect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatEffect : MonoBehaviour {
+	public float startY = 0f;
+	public float duration = 1f;
+
+	void Start () {
+		startY = transform.position.y;	
+	}
+	
+	void Update () {
+		float newY = startY + (startY + Mathf.Cos(Time.time / duration * 2)) / 4;	
+		transform.position = new Vector2(transform.position.x, newY);
+	}
+}
